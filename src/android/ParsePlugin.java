aafb6574b1ddfba193e3dceb900a9df27a3a6388
Liam Donaghy <liamdonaghy@Liams-MacBook-Pro.local>
2016-05-24 14:26:12 +0100

    New plugin

--- src/android/ParsePlugin.java
+++ src/android/ParsePlugin.java
@@ -2,17 +2,14 @@ package org.apache.cordova.core;
 
 import android.app.Application;
 import android.util.Log;
-
-import java.util.Set;
-
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-
-import com.parse.Parse;
-import com.parse.ParseInstallation;
-import com.parse.PushService;
+import java.util.List;
 
 public class ParsePlugin extends CordovaPlugin {
     public static final String TAG = "ParsePlugin";
@@ -22,7 +19,7 @@ public class ParsePlugin extends CordovaPlugin {
     public static final String ACTION_GET_SUBSCRIPTIONS = "getSubscriptions";
     public static final String ACTION_SUBSCRIBE = "subscribe";
     public static final String ACTION_UNSUBSCRIBE = "unsubscribe";
-
+    
     public static void initializeParseWithApplication(Application app) {
         String appId = getStringByKey(app, "parse_app_id");
         String clientKey = getStringByKey(app, "parse_client_key");
@@ -30,12 +27,12 @@ public class ParsePlugin extends CordovaPlugin {
         Log.d(TAG, "Initializing with parse_app_id: " + appId + " and parse_client_key:" + clientKey);
         Parse.initialize(app, appId, clientKey);
     }
-
+    
     private static String getStringByKey(Application app, String key) {
         int resourceId = app.getResources().getIdentifier(key, "string", app.getPackageName());
         return app.getString(resourceId);
     }
-
+    
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals(ACTION_INITIALIZE)) {
@@ -46,7 +43,7 @@ public class ParsePlugin extends CordovaPlugin {
             this.getInstallationId(callbackContext);
             return true;
         }
-
+        
         if (action.equals(ACTION_GET_INSTALLATION_OBJECT_ID)) {
             this.getInstallationObjectId(callbackContext);
             return true;
@@ -65,17 +62,46 @@ public class ParsePlugin extends CordovaPlugin {
         }
         return false;
     }
-
+    
     private void initialize(final CallbackContext callbackContext, final JSONArray args) {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
-            PushService.setDefaultPushCallback(cordova.getActivity(), cordova.getActivity().getClass());
-            ParseInstallation.getCurrentInstallation().saveInBackground();
-            callbackContext.success();
+                
+                try
+                {
+                    Parse.initialize(new Parse.Configuration.Builder(cordova.getActivity())
+                                     .applicationId("EWVn1O9MYRPjbTmwXKnGH3Vht52wQ5Mw7JeNsGt9")
+                                     .clientKey("LVReR0mRHrAVkIGXwSRm22XkaubHLjgb4QxZnHxp")
+                                     .server("http://parseserver-dm5pm-env.us-east-1.elasticbeanstalk.com/parse/")
+                                     .build()
+                                     );
+                    
+                    ParseInstallation.getCurrentInstallation().saveInBackground();
+                    
+                    /* for debubbling NOTE add / to end of parse url
+                     
+                     ParsePush.subscribeInBackground("", new SaveCallback() {
+                     @Override
+                     public void done(ParseException e) {
+                     if (e == null) {
+                     Log.d("com.parse.push",
+                     "successfully subscribed to the broadcast channel.");
+                     } else {
+                     Log.e("com.parse.push", "failed to subscribe for push", e);
+                     }
+                     }
+                     });
+                     */
+                    
+                    callbackContext.success();
+                }
+                catch (Exception e) {
+                    System.err.println("Caught IOException: " + e.getMessage());
+                }
             }
         });
     }
-
+    
     private void getInstallationId(final CallbackContext callbackContext) {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
@@ -84,7 +110,7 @@ public class ParsePlugin extends CordovaPlugin {
             }
         });
     }
-
+    
     private void getInstallationObjectId(final CallbackContext callbackContext) {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
@@ -93,33 +119,62 @@ public class ParsePlugin extends CordovaPlugin {
             }
         });
     }
-
+    
     private void getSubscriptions(final CallbackContext callbackContext) {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
-                 Set<String> subscriptions = PushService.getSubscriptions(cordova.getActivity());
-                 callbackContext.success(subscriptions.toString());
+                
+                List<String> list = ParseInstallation.getCurrentInstallation().getList("channels");
+                
+                //ParseInstallation.getCurrentInstallation().getObjectId().get
+                
+                try
+                {
+                    callbackContext.success(list.toString());
+                    
+                }
+                catch (Exception e) {
+                    System.err.println("Caught IOException: " + e.getMessage());
+                }
             }
         });
     }
-
+    
     private void subscribe(final String channel, final CallbackContext callbackContext) {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
-                PushService.subscribe(cordova.getActivity(), channel, cordova.getActivity().getClass());
-                callbackContext.success();
+                
+                
+                try
+                {
+                    ParsePush.subscribeInBackground(channel);
+                    callbackContext.success();
+                    
+                }
+                catch (Exception e) {
+                    System.err.println("Caught IOException: " + e.getMessage());
+                }
             }
         });
     }
-
+    
     private void unsubscribe(final String channel, final CallbackContext callbackContext) {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
-                PushService.unsubscribe(cordova.getActivity(), channel);
-                callbackContext.success();
+                
+                try
+                {
+                    ParsePush.unsubscribeInBackground(channel);
+                    callbackContext.success();
+                    
+                }
+                catch (Exception e) {
+                    System.err.println("Caught IOException: " + e.getMessage());
+                }
+                
+                
             }
         });
     }
-
 }
 
