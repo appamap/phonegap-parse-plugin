e27f490b3d368a695bdd458d2b44d951fcedd8d2
stevedj <djojokusumo.stevanus@gmail.com>
2017-10-25 20:58:58 +0700

    fix parse plugin

--- src/android/App.java
+++ src/android/App.java
@@ -1,15 +1,35 @@
 package org.apache.cordova.core;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
+import com.parse.Parse;
+
 public class App extends Application {
+
+
+    private static Context mContext;
+    private static App sInstance = null;
+
+    public static App getInstance() {
+        return sInstance;
+    }
+
+    public static Context getContext() {
+        //  return instance.getApplicationContext();
+        return mContext;
+    }
+
+
+
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext = getApplicationContext();
+        Parse.App=this;
         Log.w("init","initialize parse with application");
         ParsePlugin.initializeParseWithApplication(this);
     }
-
 }
